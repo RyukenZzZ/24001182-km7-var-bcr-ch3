a84dca93197c45d53fa1991a0e5bc211086b3719
“RyukenZzZ” <zxryuken@gmail.com>
2024-10-11 01:50:01 +0700

    Add Message Succesfull Operation

--- src/controllers/cars.js
+++ src/controllers/cars.js
@@ -14,14 +14,14 @@ exports.getCarById = (req, res, next) => {
     // Get the id from params
     const { id } = req.params;
     const data = carService.getCarById(id);
-    successResponse(res, data);
+    successResponse(res, data, "Car found successfully.");
 };
 
 exports.createCar = async (req, res, next) => {
 
     // Create the new Car
     const data = await carService.createCar(req.body, req.files);
-    successResponse(res, data);
+    successResponse(res, data, "Car successfully added!");
 };
 
 
@@ -29,12 +29,12 @@ exports.updateCar = (req, res, next) => {
     // Get the id from params
     const { id } = req.params;
     const data = carService.updateCar(id, req.body);
-    successResponse(res, data);
+    successResponse(res, data, "car successfully changed !");
 };
 
 exports.deleteCarById = (req, res, next) => {
     // Get the id from params
     const { id } = req.params;
     const data = carService.deleteCarById(id);
-    successResponse(res, data);
+    successResponse(res, data, "Car succesfully deleted !");
 };
